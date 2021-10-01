7b01a29759ed4078366c757a512dd0ca8d30ba93
unknown <anthony.n.chapman@gmail.com>
2021-10-01 15:31:35 -0400

    removed debugging statement

--- productivity-youtube/remove-content.js
+++ productivity-youtube/remove-content.js
@@ -1,9 +1,5 @@
-alert('test');
-    const sidebar = document.getElementById("secondary");
-    sidebar.parentNode.removeChild(sidebar);
+const sidebar = document.getElementById("secondary");
+sidebar.parentNode.removeChild(sidebar);
 
-   // const commentsContents = document.getElementById("contents");
-    //commentsContents.parentNode.removeChild(commentsContents);
-
-    const commentsSections = document.getElementById("comments");
-    commentsSections.parentNode.removeChild(commentsSections);
+const commentsSections = document.getElementById("comments");
+commentsSections.parentNode.removeChild(commentsSections);
