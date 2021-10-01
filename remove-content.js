90b2d834c0b5b8bf6eeb6b6b6788c12ed702716f
unknown <anthony.n.chapman@gmail.com>
2021-10-01 15:25:51 -0400

    created content script and manifest

---
+++ remove-content.js
@@ -0,0 +1,9 @@
+alert('test');
+    const sidebar = document.getElementById("secondary");
+    sidebar.parentNode.removeChild(sidebar);
+
+   // const commentsContents = document.getElementById("contents");
+    //commentsContents.parentNode.removeChild(commentsContents);
+
+    const commentsSections = document.getElementById("comments");
+    commentsSections.parentNode.removeChild(commentsSections);
